26d7de203f7d914e37fea753ab71a659779843a6
Lenita-del <lenitamontheiro@gmail.com>
2021-03-22 19:59:31 +0530

    committed again

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -2,7 +2,12 @@ package main;
 
 public class Calculator {
 	
-	 public static void main(String []args){
+	/* public static void main(String []args){
 		    System.out.println("Hello World");
-		  }
+		    
+}*/
+	 
+	 public static void add(int num1,int num2){
+	      System.out.println(num1+num2);
+	  }
 }
