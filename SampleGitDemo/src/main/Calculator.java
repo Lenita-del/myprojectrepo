1cd49514dd10efaa31075d28b26601ff41542b33
Lenita <Lenita@LAPTOP-9FD9NN0E>
2021-03-23 11:33:55 +0530

    Added br1 function in br1(remote merge)

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -20,6 +20,8 @@ public class Calculator {
 	 public static void square(int num1){
 	      System.out.println(num1*num1);
 	  }
-	 
+	 public static void br1(int num1){
+	      System.out.println(num1*num1);
+	  } 
 	 
 }
